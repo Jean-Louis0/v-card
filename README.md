f527c4aa1d707c9de09e5905a09ff072692f1e6b
root <root@JL-laptop.localdomain>
2023-05-19 00:28:45 +0200

    Full project v-card

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # v-card
+
+This is to reproduce the v-card.png picture in the folder.
---
+++ style.js
@@ -0,0 +1,13 @@
+//view on full view mode
+
+//inspired by this shot 
+//https://dribbble.com/shots/2517640-Personal-VCard
+
+
+
+//logo created with :
+//Method Draw - http://github.com/duopixel/Method-Draw/ -->
+
+
+//overlay menu forked from 
+//https://codepen.io/hexagoncircle/pen/OJLMgYY
